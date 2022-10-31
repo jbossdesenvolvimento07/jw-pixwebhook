22cc67b4f2c135f6531c8b99e47823869042a65e
JBOSS - Vitor Bersani <desenvolvimento07@jboss.srv.br>
2022-10-31 08:26:24 -0300

    voltando alterações

--- index.js
+++ index.js
@@ -23,31 +23,19 @@ app.post('/webhook/pix', (req, res) => {
     res.sendStatus(200)
 })
 
-// app.listen(process.env.PORT || 3000, function(){
-//     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-//   });
-
-/*https.createServer({
-      // ...
-      cert: fs.readFileSync('CARoot.crt'),
-      //key: fs.readFileSync('server.key'),
-      // ...
-    },
-    app
-  )
-  .listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-    });*/
+  });
 
-const options = {
-    key: fs.readFileSync(__dirname + '/ARQUIVO_CHAVE_PRIVADA.key', 'utf8'),
-    cert: fs.readFileSync(__dirname + '/NovoCertificado.crt', 'utf8')
-};
+// const options = {
+//     key: fs.readFileSync(__dirname + '/ARQUIVO_CHAVE_PRIVADA.key', 'utf8'),
+//     cert: fs.readFileSync(__dirname + '/NovoCertificado.crt', 'utf8')
+// };
 
-console.log("options", options)
+// console.log("options", options)
      
-var server = http.createServer(options, app);
+// var server = http.createServer(options, app);
 
-server.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port " + this.address().port);
-});
+// server.listen(process.env.PORT || 3000, function(){
+//     console.log("Express server listening on port " + this.address().port);
+// });
